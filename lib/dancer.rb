d0fbb3357c2df5525e8343382a7696223d6ab19d
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-08-22 14:24:32 +0300

    commit tree d7e216fb10e53ebf89840f4f2454393523177d80

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,14 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance.rb'
+
+class Dancer
+    include FancyDance::InstanceMethods  #All the methods here will be used by the including class as instance methods.
+    extend FancyDance::ClassMethods  #the methods here will be lend as class methods.
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
