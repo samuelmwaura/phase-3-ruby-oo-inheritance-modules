31fee995230b95f22b5172f213d26240a2dc42ab
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-08-22 14:37:06 +0300

    completed the lab

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,15 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance.rb'
+
+class Kid
+ include FancyDance::InstanceMethods  #signals inclusion of all instance methods
+ extend  FancyDance::ClassMethods #extends refers to class methods
+
+ attr_accessor :name
+ 
+ def initialize(name)
+    @name = name
+ end
+ 
+end
